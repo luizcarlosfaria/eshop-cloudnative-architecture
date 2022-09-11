3f10351f4a516758c2406292ad00a11c9217ac90
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2022-09-11 03:50:58 -0300

    Adicionando extensões para Bind com Factory e ConfigureWith (bind fluente)

---
+++ eShopCloudNative.Architecture/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopCloudNative.Architecture.Extensions;
+public static class ConfigurationExtensions
+{
+    public static T CreateInstanceAndConfigure<T>(this IConfiguration configuration, string key)
+        => configuration.ConfigureWith(key, Activator.CreateInstance<T>());
+
+    public static T ConfigureWith<T>(this IConfiguration configuration, string key, T item)
+    {
+        configuration.Bind(key, item);
+        return item;
+    }
+}
