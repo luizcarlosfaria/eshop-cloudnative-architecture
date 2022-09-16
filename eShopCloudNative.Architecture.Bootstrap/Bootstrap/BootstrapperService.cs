79a428252662b48b437f86923d21a0cc2d7158df
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2022-09-16 01:08:01 -0300

    Adicionando eventos de pré e pós execução no BootstrapperService, e tratamento de exceção nas implementações de métodos de IHostedService (pois ele está falhando silenciosamente em debug)

--- eShopCloudNative.Architecture.Bootstrap/Bootstrap/BootstrapperService.cs
+++ eShopCloudNative.Architecture.Bootstrap/Bootstrap/BootstrapperService.cs
@@ -1,6 +1,7 @@
 ﻿using Ardalis.GuardClauses;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,21 @@ using System.Threading.Tasks;
 namespace eShopCloudNative.Architecture.Bootstrap;
 public class BootstrapperService : IBootstrapperService, IHostedService
 {
+    public event EventHandler BeforeInitialize;
+
+    public event EventHandler AfterInitialize;
+
+    public event EventHandler BeforeExecute;
+
+    public event EventHandler AfterExecute;
+
     public List<IBootstrapperService> Services { get; set; }
 
     public async Task InitializeAsync()
     {
         Guard.Against.Null(this.Services, nameof(this.Services));
+
+        this.BeforeInitialize?.Invoke(this, EventArgs.Empty);
 
         foreach (var service in this.Services)
         {
@@ -22,11 +33,15 @@ public class BootstrapperService : IBootstrapperService, IHostedService
 
             await service.InitializeAsync();
         }
+
+        this.AfterInitialize?.Invoke(this, EventArgs.Empty);
     }
 
     public async Task ExecuteAsync()
     {
         Guard.Against.Null(this.Services, nameof(this.Services));
+
+        this.BeforeExecute?.Invoke(this, EventArgs.Empty);
 
         foreach (var service in this.Services)
         {
@@ -34,12 +49,31 @@ public class BootstrapperService : IBootstrapperService, IHostedService
 
             await service.ExecuteAsync();
         }
+
+        this.AfterExecute?.Invoke(this, EventArgs.Empty);
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        await this.InitializeAsync();
-        await this.ExecuteAsync();
+        try
+        {
+            await this.InitializeAsync();
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Erro ao executar BootstrapperService.InitializeAsync()");
+            throw;
+        }
+
+        try
+        {
+            await this.ExecuteAsync();
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Erro ao executar BootstrapperService.ExecuteAsync()");
+            throw;
+        }
     }
 
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
