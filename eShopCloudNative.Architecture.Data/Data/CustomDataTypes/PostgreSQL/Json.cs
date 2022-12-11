af389add98171ec3626e979d53354f5af323b31c
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2022-12-10 23:49:51 -0300

    Adicionando suporte à colunas JSON

---
+++ eShopCloudNative.Architecture.Data/Data/CustomDataTypes/PostgreSQL/Json.cs
@@ -0,0 +1,96 @@
+﻿using NHibernate.Engine;
+using NHibernate.SqlTypes;
+using NHibernate.UserTypes;
+using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace eShopCloudNative.Architecture.Data.CustomDataTypes.PostgreSQL;
+
+public class Json : IUserType
+{
+    public virtual SqlType[] SqlTypes => new SqlType[] {
+        new NpgSqlType(DbType.Object, NpgsqlDbType.Json)
+    };
+
+    public Type ReturnedType => typeof(JToken);
+
+    public bool IsMutable => true;
+
+    public object Assemble(object cached, object owner)
+    {
+        return cached;
+    }
+
+    public object DeepCopy(object value)
+    {
+        if (value == null)
+            return null;
+        string json = JsonConvert.SerializeObject(value);
+        object obj = JsonConvert.DeserializeObject(json, value.GetType());
+        return obj;
+    }
+
+    public object Disassemble(object value)
+    {
+        return value;
+    }
+
+    public new bool Equals(object x, object y)
+    {
+        if (x == null && y == null)
+            return true;
+        if (x == null || y == null)
+            return false;
+        if (x is JToken token1 && y is JToken token2)
+            return token1.Equals(token2);
+        return x.Equals(y);
+    }
+
+    public int GetHashCode(object x)
+    {
+        return x?.GetHashCode() ?? 0;
+    }
+
+    public object NullSafeGet(
+        DbDataReader rs,
+        string[] names,
+        ISessionImplementor session,
+        object owner
+    )
+    {
+        if (names.Length != 1)
+        {
+            throw new InvalidOperationException(
+                "Only expecting one column..."
+            );
+        }
+        if (rs[names[0]] is string val)
+            return JToken.Parse(val);
+        return null;
+    }
+
+    public void NullSafeSet(
+        DbCommand cmd,
+        object value,
+        int index,
+        ISessionImplementor session
+    )
+    {
+        var parameter = (NpgsqlParameter)cmd.Parameters[index];
+        parameter.Value = value ?? DBNull.Value;
+    }
+
+    public object Replace(object original, object target, object owner)
+    {
+        return original;
+    }
+}
---
+++ eShopCloudNative.Architecture.Data/Data/CustomDataTypes/PostgreSQL/NpgSqlType.cs
@@ -0,0 +1,33 @@
+﻿using NHibernate.SqlTypes;
+using NpgsqlTypes;
+using System.Data;
+
+namespace eShopCloudNative.Architecture.Data.CustomDataTypes.PostgreSQL;
+
+/// <summary>
+/// https://github.com/beginor/nhibernate-extensions/tree/master/src/NHibernate.Extensions.NpgSql
+/// </summary>
+public class NpgSqlType : SqlType
+{
+    public NpgsqlDbType NpgDbType { get; }
+
+    public NpgSqlType(DbType dbType, NpgsqlDbType npgDbType)
+        : base(dbType)
+    {
+        this.NpgDbType = npgDbType;
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public NpgSqlType(DbType dbType, NpgsqlDbType npgDbType, int length)
+        : base(dbType, length)
+    {
+        this.NpgDbType = npgDbType;
+    }
+
+    // ReSharper disable once UnusedMember.Global
+    public NpgSqlType(DbType dbType, NpgsqlDbType npgDbType, byte precision, byte scale)
+        : base(dbType, precision, scale)
+    {
+        this.NpgDbType = npgDbType;
+    }
+}
